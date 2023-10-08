83c8b92d7ceba9bc1125c0c818360c75822a8cf2
Faheem Iftikhar <50404373+RanaFI25@users.noreply.github.com>
2023-10-08 18:41:50 +0200

    Led 3 Blinking

--- src/main.c
+++ src/main.c
@@ -24,4 +24,25 @@ void Blink_LED1(void)
         
     } 
 }
+
+void Blink_LED3(void)
+{
+    if(!device_is_ready(gpio_ct_dev)){
+        return;
+    }
+    int ret; 
+    ret = gpio_pin_configure(gpio_ct_dev, 15, GPIO_OUTPUT_ACTIVE);
+    if(ret != 0){
+        return;
+    }
+    while(true){
+        ret = gpio_pin_set_raw(gpio_ct_dev, 15, 0); // Set
+        k_msleep(200);
+
+        ret = gpio_pin_set_raw(gpio_ct_dev, 15, 1); // Clear
+        k_msleep(200);
+        
+    } 
+}
 K_THREAD_DEFINE(Blink_LED1_ID, MY_STACK_SIZE, Blink_LED1, NULL, NULL, NULL, 0, 0, 0);
+K_THREAD_DEFINE(Blink_LED3_ID, MY_STACK_SIZE, Blink_LED3, NULL, NULL, NULL, 0, 0, 0);
